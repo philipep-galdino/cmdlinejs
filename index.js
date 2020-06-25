a466673bd945fcd616b113317efc83b52fb3ff10
Philipe Galdino <philipep.galdino@gmail.com>
2020-06-25 03:21:53 -0300

    init node cli and added inquirer for prompting user input

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
+
+clear();
+
+console.log(
+    chalk.green(
+        figlet.textSync('SuperInit', { horizontalLayout: 'full' })
+    )
+);
+
+// checks if the directory is a git repository 
+
+//if (files.directoryExists('.git')) {
+//    console.log(chalk.red('Already a git repository!'));
+//    process.exit();
+//}
